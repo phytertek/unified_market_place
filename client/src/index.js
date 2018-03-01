b5a01f0185be6e9bb07f3572f0b9545c07081643
phytertek <ryan.phytertek@gmail.com>
2018-03-01 07:23:50 -0500

    * index.js   - add material-ui setup   - add redux / redux router setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux';
+import { MuiThemeProvider } from 'material-ui/styles';
+import { Provider } from 'react-redux';
+
+import theme from './theme';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+const history = createHistory();
+const { store } = createStore(history);
+
+// Render app
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
