91faa29633cd93ab3977bcceb0277c5d94849b76
phytertek <ryan.phytertek@gmail.com>
2018-03-10 15:19:19 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -6,9 +6,7 @@ enmapi.server.setConfig({
   Host: process.env.HOST,
   Port: process.env.PORT,
   DatabaseName: process.env.DBNAME,
-  DatabaseURI:
-    process.env.DB_URI ||
-    'mongodb://hxRIGtgQ2qWBZxQbigmcsg0fyDf6pese:EGhWm48B8tMQuMu0e7RfN3nnwnV57LpW@ds012578.mlab.com:12578/unified-market-place'
+  DatabaseURI: process.env.DB_URI
 });
 
 enmapi.server.start();
